c5c08b7c498ca843abc68c27154cdb8ba445faee
novrin <106842821+novrin@users.noreply.github.com>
2024-01-02 21:47:17 -0800

    Remove Middleware type

--- access_logger.go
+++ access_logger.go
@@ -21,9 +21,9 @@ func (rw *responseWriter) WriteHeader(statusCode int) {
 	rw.ResponseWriter.WriteHeader(statusCode)
 }
 
-// AccessLogger returns a Middleware that uses slog logger to track request and
+// AccessLogger returns a middleware that uses slog logger to track request and
 // response details.
-func AccessLogger(logger *slog.Logger, prefix string) Middleware {
+func AccessLogger(logger *slog.Logger, prefix string) func(h http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			start := time.Now()
